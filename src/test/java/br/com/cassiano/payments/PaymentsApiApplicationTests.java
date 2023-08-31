da80f63f3ad0fc69ce622255f6c93383ac81cc4c
juancassiano <juancassiano@hotmail.com>
2023-08-31 07:03:32 -0300

    feat: initial commit

---
+++ src/test/java/br/com/cassiano/payments/PaymentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cassiano.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
