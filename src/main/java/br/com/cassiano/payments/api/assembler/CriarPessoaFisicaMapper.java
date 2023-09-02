2978b16839d91393c16921c93c626dbea30be97e
juancassiano <juancassiano@hotmail.com>
2023-09-02 13:38:23 -0300

    refactor: create PessoaFisicaMapper

--- src/main/java/br/com/cassiano/payments/api/assembler/CriarPessoaFisicaMapper.java
+++ src/main/java/br/com/cassiano/payments/api/assembler/CriarPessoaFisicaMapper.java
@@ -1,14 +1,14 @@
 package br.com.cassiano.payments.api.assembler;
 
 import br.com.cassiano.payments.api.model.request.PessoaFisicaRequestDTO;
-import br.com.cassiano.payments.api.model.response.PessoaJuridicaDTO;
+import br.com.cassiano.payments.api.model.response.PessoaFisicaResponseDTO;
 import br.com.cassiano.payments.domain.model.PessoaFisica;
 
 
 public class CriarPessoaFisicaMapper {
 
-    public PessoaJuridicaDTO toResponseDTO(PessoaFisica pessoaFisica) {
-        return new PessoaJuridicaDTO(pessoaFisica.getId(), pessoaFisica.getDocumento(), pessoaFisica.getCartao(), pessoaFisica.getNome());
+    public PessoaFisicaResponseDTO toResponseDTO(PessoaFisica pessoaFisica) {
+        return new PessoaFisicaResponseDTO(pessoaFisica.getId(), pessoaFisica.getDocumento(), pessoaFisica.getCartao(), pessoaFisica.getNome());
     }
 
     public PessoaFisica toPessoaFisicaEntity(PessoaFisicaRequestDTO requestDTO) {
